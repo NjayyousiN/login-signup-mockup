542aacf5b13b638ff2ae443d205d2d079989fc14
NjayyousiN <nasseraljayyousi@gmail.com>
2024-08-22 02:44:36 +0300

    Added a new font-style

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,9 +9,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{
-      children}
-      <Toaster />
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" />
+      </head>
+      <body>
+        {children}
+        <Toaster />
       </body>
     </html>
   );
